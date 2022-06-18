4bff17c8a3b5ec44394299d1d13cbf5e6058bd74
Karlheinz <levkad19@htl-kaindorf.at>
2022-06-18 20:41:24 +0200

    Piet Hein

--- js/main.js
+++ js/main.js
@@ -13,7 +13,8 @@ var myPix = ["img/cards/2_of_clubs.png",        "img/cards/2_of_diamonds.png",  
              "img/cards/jack_of_clubs.png",     "img/cards/jack_of_diamonds.png",  "img/cards/jack_of_hearts.png",  "img/cards/jack_of_spades.png",
              "img/cards/king_of_clubs.png",     "img/cards/king_of_diamonds.png",  "img/cards/king_of_hearts.png",  "img/cards/king_of_spades.png",
              "img/cards/queen_of_clubs.png",    "img/cards/queen_of_diamonds.png", "img/cards/queen_of_hearts.png", "img/cards/queen_of_spades.png"];
-
+var refill=[];
+refill=myPix;
 var checks=["1","1","1","1",
             "2","2","2","2",
             "3","3","3","3",
@@ -32,6 +33,10 @@ var inhalt=[];
 var flush=[];
 function showcard() {
 
+    if(myPix.length<=6){
+        console.log(myPix.length);
+        myPix=refill;
+    }
     document.getElementById("pair").style.background="transparent";
     document.getElementById("twoPair").style.background="transparent";
     var randomNum = Math.floor(Math.random() * myPix.length);
@@ -70,31 +75,56 @@ function showcard() {
     console.log(inhalt);
     checks.splice(randomNum,1);
 
+    randomNum = Math.floor(Math.random() * myPix.length);
+    document.getElementById("tableCard4").src = myPix[randomNum];
+    myPix.splice(randomNum,1);
+    inhalt.push(checks[randomNum]);
+    flush.push(randomNum);
+    console.log(inhalt);
+    checks.splice(randomNum,1);
+
+    randomNum = Math.floor(Math.random() * myPix.length);
+    document.getElementById("tableCard5").src = myPix[randomNum];
+    myPix.splice(randomNum,1);
+    inhalt.push(checks[randomNum]);
+    flush.push(randomNum);
+    console.log(inhalt);
+    checks.splice(randomNum,1);
+
     document.getElementById("theeOfaKind").style.background="transparent";
     document.getElementById("pair").style.background="transparent";
     document.getElementById("twoPair").style.background="transparent";
-
-    var abfrage=threeOfaKind();
-    flush1();
-    if(straightFlush()==true){
-        console.log(inhalt);
-        document.getElementById("straightFlush").style.background="rgb(255, 0, 0)";
+    document.getElementById("flush").style.background="transparent";
+    if(RoyalFlush==true){
+        document.getElementById("royalFlush").style.background="rgb(255, 0, 0)";
     }
-    if(straight()==true){
-        console.log(inhalt);
+    if(fourOfaKind()==true){
+        document.getElementById("fourOfaKind").style.background="rgb(255, 0, 0)";
+    }
+    else if(flush1()==true){
+        document.getElementById("flush").style.background="rgb(255, 0, 0)";
+    }
+    else if(straight()==true){
         document.getElementById("straight").style.background="rgb(255, 0, 0)";
     }
+
+
+    let abfrage=threeOfaKind();
+    let pairC=pair();
+    if(abfrage==true && pairC==true){
+        document.getElementById("fullHouse").style.background="rgb(255, 0, 0)";
+    }
+    if(straightFlush()==true){
+        document.getElementById("straightFlush").style.background="rgb(255, 0, 0)";
+    }
     if(abfrage==true){
-        console.log(inhalt);
         document.getElementById("theeOfaKind").style.background="rgb(255, 0, 0)";
     }
-   if(pair()==true&&abfrage!=true){
-        console.log(inhalt);
+    if(pairC==true&&abfrage!=true){
         document.getElementById("pair").style.background="rgb(255, 0, 0)";
 
     }
     if(DoublePair() == true&&abfrage!=true){
-        console.log(inhalt);
         document.getElementById("twoPair").style.background="rgb(255, 0, 0)";
 
     }
@@ -103,6 +133,55 @@ function showcard() {
         //check(randomNum,randomNum2,randomNum3,randomNum4,randomNum5);
 
 }
+function RoyalFlush(){
+    flush.sort((a, b) => a - b);
+    inhalt.sort((a, b) => a - b);
+    let i=0;
+    let c=0;
+    let check=0;
+    let check2=0;
+    let check3=1;
+    while(i<=inhalt.length){
+        c=i;
+        if(inhalt[i]==9){
+            check=flush[i];
+            while(c<=flush.length){
+                check2=flush[c+i];
+                if(check==check2+4){
+                    check3++;
+                    if(check3==5){
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false
+}
+function fourOfaKind(){
+    let i=0;
+    let c=1;
+    let c2=1;
+    while(i<=inhalt.length){
+        while(c<=inhalt.length){
+            if(inhalt[i]===inhalt[c]){
+                c2++;
+                console.log(c2,inhalt);
+                if(c2==4){
+                    i=1;
+                    while(i<=4){
+                        i++;
+                    }
+                    console.log(inhalt);
+                    return true;
+                }
+            }
+            c++;
+        }
+        i++;
+    }
+    return false;
+}
 function straightFlush(){
     flush.sort((a, b) => a - b);
     console.log(flush);
@@ -110,12 +189,12 @@ function straightFlush(){
     let check2;
     let i=0;
     let c=0;
-    let check3=1;
+    let check3=0;
     while(i<=flush.length){
         c=i;
-        check1=flush[i]
-        while(c<=flush.length){
-            check2=flush[c+1]
+        check1=flush[i];
+        while(c<flush.length){
+            check2=flush[c+1];
             if(check2==check1+4){
                 check3++;
                 if(check3==5){
@@ -134,7 +213,6 @@ function straightFlush(){
 function flush1(){
     flush.sort((a, b) => a - b);
     console.log(flush);
-    flush=[12,36,72,144,288];
     let c=0;
     let mod;
     let i;
@@ -149,11 +227,9 @@ function flush1(){
         }
         else if(flush[c]%4==0){
             mod=4;
-        }else{
-            mod=1
         }
         while(i<=flush.length){
-            if(flush[i+1]%mod==0){
+            if(flush[i+1]%mod==0&&flush[i+1]!=flush[i]){
                 check++;
                 console.log(check,mod)
                 if(check==5){
@@ -196,16 +272,23 @@ function straight(){
 
 }
 function threeOfaKind(){
-    console.log(inhalt);
     let i=0;
     let c=1;
     let c2=1;
+    var safe=[];
     while(i<=inhalt.length){
         while(c<=inhalt.length){
             if(inhalt[i]===inhalt[c]){
                 c2++;
-                console.log(c2);
+                safe.push(c);
+                console.log(c2,inhalt);
                 if(c2==3){
+                    i=1;
+                    while(i<=3){
+                        inhalt.splice(safe[i],1);
+                        i++;
+                    }
+                    console.log(inhalt);
                     return true;
                 }
             }
