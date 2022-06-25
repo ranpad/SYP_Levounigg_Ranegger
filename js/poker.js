3a91a0079729515cc7d743e12e16b53b797f2411
ranpad19 <ranpad19@htl-kaindorf.at>
2022-06-25 10:15:59 +0200

    1

--- js/poker.js
+++ js/poker.js
@@ -5,17 +5,20 @@ var balancePlayer = 500;
 var balanceBot1 = 500;
 var balanceBot2 = 500;
 var balanceBot3 = 500;
+var round = 0;
+var isFold = false;
+
 
 function main(){
+    round++;
     updateBetButtons();
-    getBet();
 }
 
 function allIn(){
     totalPot += balancePlayer;
     balancePlayer = 0;
     document.getElementById("balance").textContent="Balance:" + balancePlayer + " $";
-    document.getElementById("totalPot").textContent="Total Pot: " + totalPot + " $"
+    document.getElementById("totalPot").textContent="Total Pot: " + totalPot + " $";
 }
 
 function bet(){
@@ -41,12 +44,16 @@ function updateBetButtons(){
 
 function getBet(){
     document.getElementById("bet").onclick = function (){balancePlayer -= currentTotalBet; totalPot += currentTotalBet;}
-    document.getElementById("bet1").onclick = function (){currentBet = document.getElementById("bet1").innerHTML.substring(0, 3); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
-    document.getElementById("bet2").onclick = function (){currentBet = document.getElementById("bet2").innerHTML.substring(0, 3); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
-    document.getElementById("bet3").onclick = function (){currentBet = document.getElementById("bet3").innerHTML.substring(0, 3); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
-    document.getElementById("bet4").onclick = function (){currentBet = document.getElementById("bet4").innerHTML.substring(0, 3); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
-    document.getElementById("bet5").onclick = function (){currentBet = document.getElementById("bet5").innerHTML.substring(0, 3); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
+    document.getElementById("bet1").onclick = function (){currentBet = document.getElementById("bet1").innerHTML.substring(0, 3); currentBet = parseInt(currentBet); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
+    document.getElementById("bet2").onclick = function (){currentBet = document.getElementById("bet2").innerHTML.substring(0, 3); currentBet = parseInt(currentBet); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
+    document.getElementById("bet3").onclick = function (){currentBet = document.getElementById("bet3").innerHTML.substring(0, 3); currentBet = parseInt(currentBet); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
+    document.getElementById("bet4").onclick = function (){currentBet = document.getElementById("bet4").innerHTML.substring(0, 3); currentBet = parseInt(currentBet); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
+    document.getElementById("bet5").onclick = function (){currentBet = document.getElementById("bet5").innerHTML.substring(0, 3); currentBet = parseInt(currentBet); totalPot += currentBet; balancePlayer-=currentBet; updateBalanceAndPot(totalPot, balancePlayer)}
+    console.log(totalPot);
+    console.log(balancePlayer);
+
 }
+
 
 function updateCurrentBalance(){
 
@@ -54,5 +61,29 @@ function updateCurrentBalance(){
 
 function updateBalanceAndPot(totalPot, balancePlayer){
     document.getElementById("balance").textContent="Balance:" + balancePlayer + " $";
-    document.getElementById("totalPot").textContent="Total Pot: " + totalPot + " $"
+    document.getElementById("totalPot").textContent="Total Pot: " + totalPot + " $";
+}
+
+function disableButtons(){
+    document.getElementById('fold').disabled = true;
+    document.getElementById('check').disabled = true;
+    document.getElementById('bet').disabled = true;
+    document.getElementById('bet1').disabled = true;
+    document.getElementById('bet2').disabled = true;
+    document.getElementById('bet3').disabled = true;
+    document.getElementById('bet4').disabled = true;
+    document.getElementById('bet5').disabled = true;
+    document.getElementById('allIn').disabled = true;
+}
+
+function enableButtons(){
+    document.getElementById('fold').disabled = false;
+    document.getElementById('check').disabled = false;
+    document.getElementById('bet').disabled = false;
+    document.getElementById('bet1').disabled = false;
+    document.getElementById('bet2').disabled = false;
+    document.getElementById('bet3').disabled = false;
+    document.getElementById('bet4').disabled = false;
+    document.getElementById('bet5').disabled = false;
+    document.getElementById('allIn').disabled = false;
 }
